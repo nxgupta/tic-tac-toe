94671d709195de72fd65d8622aa702864ef395ea
Neeraj Gupta <neerajgupta6525@gmail.com>
2024-08-17 16:59:45 +0000

    completed tai-tac-toe

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,7 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
 
-let turnO = true; //playerX, playerY
+let turnO = !!Math.random(); //playerX, playerY
 let winPatterns = [
   [0, 1, 2],
   [3, 4, 5],
@@ -13,12 +13,42 @@ let winPatterns = [
   [2, 4, 6],
 ];
 
+const disableButtons = () => {
+  boxes.forEach((box) => {
+    box.disabled = true;
+  });
+  resetBtn.innerText = "New Game";
+};
+
+const enableButtons = () => {
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.disabled = false;
+  });
+  resetBtn.innerText = "Reset";
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let val1 = boxes[pattern[0]].innerText;
+    let val2 = boxes[pattern[1]].innerText;
+    let val3 = boxes[pattern[2]].innerText;
+    if (val1 !== "" && val2 !== "" && val3 !== "") {
+      if (val1 === val2 && val2 === val3) {
+        disableButtons();
+        setTimeout(() => alert(`Winner is ${val1}`, 0));
+      }
+    }
+  }
+};
+
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     box.innerText = turnO ? "X" : "O";
     turnO = !turnO;
     box.disabled = true;
+    checkWinner();
   });
 });
 
-// resetBtn.addEventListener("click");
+resetBtn.addEventListener("click", enableButtons);
