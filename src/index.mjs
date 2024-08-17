30d479b59aae5bc57208bd53fc17e10be82128a3
CodeSandbox <support@codesandbox.io>
2024-08-17 13:29:32 +0000

    Sandbox snapshot commit

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,24 @@
-import "./styles.css";
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+let turnO = true; //playerX, playerY
+let winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    box.innerText = turnO ? "X" : "O";
+    turnO = !turnO;
+    box.disabled = true;
+  });
+});
+
+// resetBtn.addEventListener("click");
